b9a622e210b3ff39839be3c7474948cc3c43270b
ddn005UCSD <103156034+ddn005UCSD@users.noreply.github.com>
2022-04-15 09:41:51 -0700

    added stuff
    Added stuff

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,10 +12,28 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+
+            int noImage = markdown.indexOf("!", currentIndex);
+            
+
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
+
+            if (openBracket == -1 || openParen == -1) {
+                break;
+            }
+
+            System.out.print("noimg: "+noImage + "openbrak: " + openBracket);
+            if (noImage == openBracket-1) {
+                System.out.println(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen;
+                continue;
+            }
+            
+           
+            
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
---
+++ test3.md
@@ -0,0 +1,13 @@
+#Big text
+
+![img](.png)
+
+[link](true link)
+
+bad1(badlink1)
+
+[bad2]badlink2
+
+![mfthisisanIMAGE](socialsecurity.png)
+
+[longlad]   (woop)
