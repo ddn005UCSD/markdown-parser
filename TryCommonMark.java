b310f1ae6513c35c796276192b49544dfb4f3c40
ddn005UCSD <103156034+ddn005UCSD@users.noreply.github.com>
2022-06-03 08:53:34 -0700

    Changed extention files

--- TryCommonMark.java
+++ TryCommonMark.java
@@ -2,7 +2,7 @@ import org.commonmark.node.*;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
 
-class TryCommonMark {
+class TryCommonMark {   
     public static void main(String[] args) {
         Parser parser = Parser.builder().build();
         Node document = parser.parse("This is *Sparta*");
